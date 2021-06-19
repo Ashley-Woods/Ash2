ac41c7485f5e0880477b5c22b68ad891f98e35df
Ashley-Woods <67157161+Ashley-Woods@users.noreply.github.com>
2021-06-20 00:03:34 +0100

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import "../styles/styles.css";
+import { Map } from "./Map";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <Map />
+    </div>
+  );
+}

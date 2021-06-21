c80d884344c6efea9ec54f3422d58446cd89d13b
Ashley-Woods <67157161+Ashley-Woods@users.noreply.github.com>
2021-06-21 19:44:50 +0100

    Before MobX

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./components/App";
-import configureStore from "./store/configureStore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import configureStore from './store/configureStore';
 //import { zoomValue } from './actions/mapActions';
 
 // create store
@@ -12,7 +12,7 @@ const store = configureStore();
 // TEMP
 store.subscribe(() => {
   const state = store.getState();
-  console.log("state subscribe: ", state);
+  console.log('state subscribe: ', state);
 });
 
 // create parent java script xml (jsx), and  pass in store, so accessible by children
@@ -22,7 +22,7 @@ const jsx = (
   </Provider>
 );
 
-console.log("Ash Running");
+console.log('Ash Running');
 console.log(store.getState);
 
-ReactDOM.render(jsx, document.getElementById("root"));
+ReactDOM.render(jsx, document.getElementById('root'));
